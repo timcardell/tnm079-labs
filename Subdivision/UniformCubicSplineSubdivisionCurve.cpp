601234fe5eb27ffe18332f509aee0bd60423dd69
Tobias Ryttlinger <tosryr@gmail.com>
2020-04-20 12:44:07 +0200

    Update UniformCubicSplineSubdivisionCurve.cpp

--- Subdivision/UniformCubicSplineSubdivisionCurve.cpp
+++ Subdivision/UniformCubicSplineSubdivisionCurve.cpp
@@ -2,55 +2,64 @@
 #include "UniformCubicSplineSubdivisionCurve.h"
 
 UniformCubicSplineSubdivisionCurve::UniformCubicSplineSubdivisionCurve(
-    const std::vector<Vector3<float>> &joints, Vector3<float> lineColor,
-    float lineWidth)
+    const std::vector<Vector3<float>> &joints, Vector3<float> lineColor, float lineWidth)
     : mCoefficients(joints), mControlPolygon(joints) {
-  this->mLineColor = lineColor;
-  this->mLineWidth = lineWidth;
+    this->mLineColor = lineColor;
+    this->mLineWidth = lineWidth;
 }
 
 void UniformCubicSplineSubdivisionCurve::Subdivide() {
-  // Allocate space for new coefficients
-  std::vector<Vector3<float>> newc;
+    // Allocate space for new coefficients
+    std::vector<Vector3<float>> newc;
 
-  assert(mCoefficients.size() > 4 && "Need at least 5 points to subdivide");
+    assert(mCoefficients.size() > 4 && "Need at least 5 points to subdivide");
+    newc.push_back(mCoefficients.front());
 
-  // Implement the subdivision scheme for a natural cubic spline here
+    // Implement the subdivision scheme for a natural cubic spline here
+    for (int i = 1; i < mCoefficients.size()-1; i++) {
+        int j = i;
+        newc.push_back( (mCoefficients.at(j--) + (6 * mCoefficients.at(j)) +mCoefficients.at(j++))/8);
+        newc.push_back((4 * mCoefficients.at(j) + 4 * mCoefficients.at(j++))/8);
 
-  // If 'mCoefficients' had size N, how large should 'newc' be? Perform a check
-  // here!
-  assert(true && "Incorrect number of new coefficients!");
+    }
 
-  mCoefficients = newc;
+    newc.push_back(mCoefficients.back());
+    // If 'mCoefficients' had size N, how large should 'newc' be? Perform a check
+    // here!
+    std::cout << "size of newc:" << newc.size() << std::endl;
+
+    // assert(newc.size() == (newc.size() * 2) - 1 && "Incorrect number of new coefficients!");
+
+    mCoefficients = newc;
 }
 
 void UniformCubicSplineSubdivisionCurve::Render() {
-  // Apply transform
-  glPushMatrix(); // Push modelview matrix onto stack
+    // Apply transform
+    glPushMatrix();  // Push modelview matrix onto stack
 
-  // Convert transform-matrix to format matching GL matrix format
-  // Load transform into modelview matrix
-  glMultMatrixf(mTransform.ToGLMatrix().GetArrayPtr());
+    // Convert transform-matrix to format matching GL matrix format
+    // Load transform into modelview matrix
+    glMultMatrixf(mTransform.ToGLMatrix().GetArrayPtr());
 
-  mControlPolygon.Render();
+    mControlPolygon.Render();
 
-  // save line point and color states
-  glPushAttrib(GL_POINT_BIT | GL_LINE_BIT | GL_CURRENT_BIT);
+    // save line point and color states
+    glPushAttrib(GL_POINT_BIT | GL_LINE_BIT | GL_CURRENT_BIT);
 
-  // draw segments
-  glLineWidth(mLineWidth);
-  glColor3fv(mLineColor.GetArrayPtr());
-  glBegin(GL_LINE_STRIP);
-  // just draw the spline as a series of connected linear segments
-  for (size_t i = 0; i < mCoefficients.size(); i++) {
-    glVertex3fv(mCoefficients.at(i).GetArrayPtr());
-  }
-  glEnd();
+    // draw segments
+    glLineWidth(mLineWidth);
+    glColor3fv(mLineColor.GetArrayPtr());
+    glBegin(GL_LINE_STRIP);
+    // just draw the spline as a series of connected linear segments
+    for (size_t i = 0; i < mCoefficients.size(); i++) {
+        glVertex3fv(mCoefficients.at(i).GetArrayPtr());
+    }
+    glEnd();
 
-  // restore attribs
-  glPopAttrib();
+    // restore attribs
+    glPopAttrib();
 
-  glPopMatrix();
+    glPopMatrix();
 
-  GLObject::Render();
+    GLObject::Render();
 }
